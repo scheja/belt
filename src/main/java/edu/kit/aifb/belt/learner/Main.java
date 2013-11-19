61db604754bf2724b4d229e934f4bc115dbef291
Jan Scheurenbrand <mail@janscheurenbrand.de>
2013-11-19 14:49:29 +0100

    Basic package layout

---
+++ src/main/java/edu/kit/aifb/belt/learner/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.kit.aifb.belt.learner;
+
+/**
+ * @author janscheurenbrand
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Let's try to learn some Qs...");
+	}
+
+}
---
+++ src/main/java/edu/kit/aifb/belt/sourceranker/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.kit.aifb.belt.sourceranker;
+
+/**
+ * @author janscheurenbrand
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Let's try to rank the sources...");
+	}
+
+}
