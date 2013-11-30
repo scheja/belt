b60878a4d250bad97ffa974aff437adee275ddb0
Sebastian Schmidt <isibboi@gmail.com>
2013-11-30 16:17:10 +0100

    Added comment explaining why we need a state without a domain.

--- src/main/java/edu/kit/aifb/belt/db/AbstractStateFactory.java
+++ src/main/java/edu/kit/aifb/belt/db/AbstractStateFactory.java
@@ -41,39 +41,44 @@ public abstract class AbstractStateFactory {
 		Iterator<Quad> quads = source.findAllByURI(url);
 		Multiset<String> properties = HashMultiset.create();
 		Set<String> type = new HashSet<String>();
-		
+
 		while (quads.hasNext()) {
 			Quad quad = quads.next();
-			
-			if (quad.getPredicate().equals("rdf:type") || quad.getPredicate().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+
+			if (quad.getPredicate().equals("rdf:type")
+					|| quad.getPredicate().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
 				type.add(quad.getPredicate().toString());
 			}
 		}
-		
+
 		URL u = null;
-		
+
 		try {
 			u = new URL(url);
 		} catch (MalformedURLException e) {
 			Logger.getLogger(getClass()).log(Level.WARN, "Malformed URL", e);
 		}
-		
+
 		return createState(u.getHost(), type, properties, source.getDictionary());
 	}
-	
+
+	// For the future, since the domain will be encoded in the action. And we
+	// don't want objects with a specific domain, but we want to find the best
+	// domain for a specific kind of object.
 	public List<State> createStateWithoutDomain(String url, Database source) {
 		Iterator<Quad> quads = source.findAllByURI(url);
 		Multiset<String> properties = HashMultiset.create();
 		Set<String> type = new HashSet<String>();
-		
+
 		while (quads.hasNext()) {
 			Quad quad = quads.next();
-			
-			if (quad.getPredicate().equals("rdf:type") || quad.getPredicate().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+
+			if (quad.getPredicate().equals("rdf:type")
+					|| quad.getPredicate().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
 				type.add(quad.getPredicate().toString());
 			}
 		}
-		
+
 		return createState(null, type, properties, source.getDictionary());
 	}
 }
